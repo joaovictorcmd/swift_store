d3160523b07e567913e0ec38bb786adf8bc3cc06
Joao Victor Carvalho Machado <machado.joaovictor@outlook.com>
2025-02-04 16:15:22 -0300

    :lock: feat: Add security dependencies and provisional security component to open endpoints

---
+++ src/main/java/com/joaovictorcmd/swiftstore/config/WebSecurityConfig.java
@@ -0,0 +1,31 @@
+package com.joaovictorcmd.swiftstore.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+/**
+ * @author joaovictorcmd
+ * @date 2025 Feb 04
+ */
+@Configuration
+public class WebSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll()
+                )
+                .headers(headers -> headers
+                        .frameOptions(HeadersConfigurer.FrameOptionsConfig::disable)
+                )
+                .csrf(AbstractHttpConfigurer::disable
+                );
+
+        return http.build();
+    }
+}
